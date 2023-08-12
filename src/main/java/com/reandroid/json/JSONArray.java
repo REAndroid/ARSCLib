26f098fbcc207ca7425c9b901bb8789cde153159
Kirlif <kirlif@tutanota.de>
2023-08-12 13:20:17 +0200

    sorting uncompressed-files.json data

--- src/main/java/com/reandroid/json/JSONArray.java
+++ src/main/java/com/reandroid/json/JSONArray.java
@@ -13,6 +13,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -697,6 +698,10 @@ public class JSONArray extends JSONItem implements Iterable<Object> {
 
     public boolean isEmpty() {
         return this.myArrayList.isEmpty();
+    }
+
+    public void sort(Comparator comparator) {
+        this.myArrayList.sort(comparator);
     }
 
     private void addAll(Collection<?> collection, boolean wrap) {
