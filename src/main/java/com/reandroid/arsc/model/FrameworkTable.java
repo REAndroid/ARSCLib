b00cb0d933dc118112d221a082c96f0a455899bb
REAndroid <REAndroid@users.noreply.github.com>
2024-06-21 22:55:00 +0200

    Clean styles before shrinking strings

--- src/main/java/com/reandroid/arsc/model/FrameworkTable.java
+++ src/main/java/com/reandroid/arsc/model/FrameworkTable.java
@@ -169,8 +169,8 @@ public class FrameworkTable extends TableBlock {
     }
     private void optimizeTableString(){
         removeUnusedTableString();
-        shrinkTableString();
         getStringPool().getStyleArray().clear();
+        shrinkTableString();
         removeUnusedTableString();
     }
     private void removeUnusedTableString(){
