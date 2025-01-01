074e7621576b324727eb45a4cc397a18da538e61
REAndroid <REAndroid@users.noreply.github.com>
2025-01-01 11:59:07 +0100

    [DEX] Use binary search algorithm to search items with offset

--- src/main/java/com/reandroid/dex/sections/DataSectionArray.java
+++ src/main/java/com/reandroid/dex/sections/DataSectionArray.java
@@ -25,55 +25,53 @@ import java.io.IOException;
 
 public class DataSectionArray<T extends DataItem> extends SectionArray<T> {
 
-    private int mStart;
-    private int mEnd;
-
     public DataSectionArray(IntegerPair countAndOffset, Creator<T> creator) {
         super(countAndOffset, creator);
     }
 
-    @Override
-    public void onChanged() {
-        super.onChanged();
-        mStart = 0;
-    }
-
-    public T getAt(int offset){
-        if(offset <= 0){
+    public T getAt(int offset) {
+        if (offset <= 0) {
             return null;
         }
-        int start = this.mStart;
-        if(start == 0 || offset < start || offset > mEnd){
-            updateBounds();
-        }
-        return getAt(offset, estimateBeginPosition(offset));
-    }
-    public T getAt(int offset, int indexHint){
-        if(offset <= 0){
-            return null;
-        }
-        T item = spiderSearch(offset, indexHint);
-        if(item != null){
-            return item;
-        }
-        //should not reach here
-        item = lazySearch(offset);
-        if(item != null){
-            updateBounds();
+        T item = binaryOffsetSearch(offset);
+        if (item == null) {
+            // should not reach here
+            // un ordered offset entries or the offset does not exist
+            item = lazyOffsetSearch(offset);
         }
         return item;
     }
-    public T[] getAt(int[] offsets){
-        if(offsets == null || offsets.length == 0){
-            return null;
+
+    private T binaryOffsetSearch(int offset) {
+        // Assumed all entries are ordered in ascending offset
+        int start = 0;
+        int end = size() - 1;
+        while (end >= start) {
+            int mid = start + ((end - start) / 2);
+            T item = get(mid);
+            int test = item.getOffset();
+            if (test == offset) {
+                return item;
+            }
+            if (test < offset) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
-        int length = offsets.length;
-        T[] results = this.newArrayInstance(offsets.length);
-        for(int i = 0; i < length; i++){
-            results[i] = getAt(offsets[i]);
-        }
-        return results;
+        return null;
     }
+    private T lazyOffsetSearch(int offset) {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            T item = get(i);
+            if (offset == item.getOffset()) {
+                return item;
+            }
+        }
+        return null;
+    }
+
     @Override
     public void readChild(BlockReader reader, T item) throws IOException {
         int offset = reader.getPosition();
@@ -85,115 +83,8 @@ public class DataSectionArray<T extends DataItem> extends SectionArray<T> {
     public void onPreRemove(T item) {
         super.onPreRemove(item);
         IntegerReference reference = item.getOffsetReference();
-        if(reference != null){
+        if (reference != null) {
             reference.set(0);
-        }
-    }
-
-    private T lazySearch(int offset){
-        int size = size();
-        for(int i = 0; i < size; i++){
-            T item = get(i);
-            if(offset == item.getOffset()){
-                return item;
-            }
-        }
-        return null;
-    }
-    private T spiderSearch(int offset, int beginIndex){
-        int size = size();
-        int i = beginIndex;
-        boolean lookBack = false;
-        boolean lookFront = false;
-        boolean lookChanged = false;
-        int step = 1;
-        int acceleration = 1;
-        while (true){
-            if(i < 0 || i >= size){
-                return null;
-            }
-            T item = get(i);
-            int itemOffset = item.getOffset();
-            if(itemOffset == offset){
-                return item;
-            }
-            if(offset < itemOffset){
-                if(lookFront){
-                    if(lookChanged){
-                        return null;
-                    }
-                    lookChanged = true;
-                    lookFront = false;
-                    step = 1;
-                }else if(lookBack && i == 0){
-                    return null;
-                }
-                if(!lookChanged){
-                    int remain = i/3 + 1;
-                    if(step > remain){
-                        step = remain;
-                    }
-                }
-                lookBack = true;
-                i = i - step;
-                if(i < 0){
-                    i = 0;
-                }
-            }else {
-                if(lookBack){
-                    if(lookChanged){
-                        return null;
-                    }
-                    lookBack = false;
-                    lookChanged = true;
-                    step = 1;
-                }
-                if(!lookChanged){
-                    int remain = (size - i)/3 + 1;
-                    if(step > remain){
-                        step = remain;
-                    }
-                }
-                lookFront = true;
-                i = i + step;
-            }
-            if(!lookChanged){
-                step += acceleration + 1;
-                if(step % 2 == 1){
-                    acceleration ++;
-                }
-            }
-        }
-    }
-    private int estimateBeginPosition(int offset){
-        int bytes = mEnd - mStart;
-        if(bytes == 0){
-            bytes = 1;
-        }
-        int offsetDiff = offset - mStart;
-        long result = (long) offsetDiff * size();
-        result = result / bytes;
-        int index = (int) result;
-        index = index - 1;
-        int size = size();
-        if(index >= size){
-            index = size - 1;
-        }
-        if(index < 0){
-            index = 0;
-        }
-        return index;
-    }
-    private void updateBounds(){
-        mStart = 0;
-        T item = getFirst();
-        if(item != null){
-            mStart = item.getOffset();
-        }
-        mEnd = mStart;
-        item = getLast();
-        if(item != null){
-            mEnd = item.getOffset();
         }
     }
 }
