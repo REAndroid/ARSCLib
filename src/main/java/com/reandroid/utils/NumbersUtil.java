52b8d33033a8611d90368c524e95b549eda1404d
REAndroid <REAndroid@users.noreply.github.com>
2025-01-21 20:26:45 +0100

    More numbers util

--- src/main/java/com/reandroid/utils/NumbersUtil.java
+++ src/main/java/com/reandroid/utils/NumbersUtil.java
@@ -17,6 +17,57 @@ package com.reandroid.utils;
 
 public class NumbersUtil {
 
+    public static int getUInt(int value, int start, int bitCount) {
+        value = value >> (start + 1 - bitCount);
+        int mask = ~(0xffffffff << bitCount);
+        return value & mask;
+    }
+    public static int setUInt(int out, int value, int start, int bitCount) {
+        int shift = (start - bitCount) + 1;
+        int mask = 0xffffffff >>> (32 - bitCount);
+        value = value & mask;
+        mask = ~(mask << shift);
+        out = out & mask;
+        value = value << shift;
+        return out | value;
+    }
+    public static int toSignedInt(int bitsCount, int unsigned) {
+        if (unsigned <= 0) {
+            return unsigned;
+        }
+        int mask = 0xffffffff >>> (32 - bitsCount);
+        unsigned = unsigned & mask;
+        int maxValue = 1 << (bitsCount - 1);
+        if (unsigned < maxValue) {
+            return unsigned;
+        }
+        return ~mask | unsigned;
+    }
+    public static long toSigned(int bitsCount, long unsigned) {
+        if (unsigned <= 0) {
+            return unsigned;
+        }
+        long mask = 0xffffffffffffffffL >>> (64 - bitsCount);
+        unsigned = unsigned & mask;
+        long maxValue = 1L << (bitsCount - 1);
+        if (unsigned < maxValue) {
+            return unsigned;
+        }
+        return ~mask | unsigned;
+    }
+    public static long maxValue(int bitsCount) {
+        if (bitsCount <= 0) {
+            return 0;
+        }
+        return 0xffffffffffffffffL >>> (65 - bitsCount);
+    }
+    public static long minValue(int bitsCount) {
+        if (bitsCount <= 0) {
+            return 0;
+        }
+        long min = 1L << (bitsCount - 1);
+        return -min;
+    }
     public static int abs(int i){
         if(i < 0){
             i = -i;
