5d56793030d2bb1ea6b01afd3d1d405d8fec33ee
REAndroid <REAndroid@users.noreply.github.com>
2025-02-24 12:20:46 +0100

    objects array to HashSet

--- src/main/java/com/reandroid/utils/collection/CollectionUtil.java
+++ src/main/java/com/reandroid/utils/collection/CollectionUtil.java
@@ -53,6 +53,10 @@ public class CollectionUtil {
         }
         return results;
     }
+    @SafeVarargs
+    public static<T> HashSet<T> asHashSet(T ... elements) {
+        return new HashSet<>(asList(elements));
+    }
 
     public static<T extends Comparable<T>> void sort(List<T> list){
         list.sort(getComparator());
