834c27318e77f6d160316e3319b06050fdfbcdcb
REAndroid <REAndroid@users.noreply.github.com>
2025-04-26 13:08:59 +0200

    Use Predicate instead of regex

--- src/main/java/com/reandroid/archive/writer/ZipAligner.java
+++ src/main/java/com/reandroid/archive/writer/ZipAligner.java
@@ -16,53 +16,70 @@
 package com.reandroid.archive.writer;
 
 import com.reandroid.archive.Archive;
-import com.reandroid.archive.block.DataDescriptor;
 import com.reandroid.archive.block.LocalFileHeader;
 import com.reandroid.archive.block.ZipHeader;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ZipAligner {
 
-    private final Map<Pattern, Integer> alignmentMap;
+    private final Map<Pattern, Integer> alignmentPatternMapDepreciated;
+    private final Map<Predicate<String>, Integer> alignmentMap;
     private int defaultAlignment;
 
-    public ZipAligner(){
+    public ZipAligner() {
+        alignmentPatternMapDepreciated = new HashMap<>();
         alignmentMap = new HashMap<>();
     }
 
-    public void setFileAlignment(Pattern patternFileName, int alignment){
-        if(patternFileName == null){
+    public void setFileAlignment(Predicate<String> predicate, int alignment) {
+        if (predicate == null) {
             return;
         }
-        alignmentMap.remove(patternFileName);
-        if(alignment > 1){
-            alignmentMap.put(patternFileName, alignment);
+        alignmentMap.remove(predicate);
+        if (alignment > 1) {
+            alignmentMap.put(predicate, alignment);
         }
     }
-    public void clearFileAlignment(){
+
+    /**
+     * Use: setFileAlignment(Predicate<String>, int)
+     * */
+    @Deprecated
+    public void setFileAlignment(Pattern patternFileName, int alignment) {
+        if (patternFileName == null) {
+            return;
+        }
+        alignmentPatternMapDepreciated.remove(patternFileName);
+        if (alignment > 1) {
+            alignmentPatternMapDepreciated.put(patternFileName, alignment);
+        }
+    }
+    public void clearFileAlignment() {
         alignmentMap.clear();
+        alignmentPatternMapDepreciated.clear();
     }
     public void setDefaultAlignment(int defaultAlignment) {
-        if(defaultAlignment <= 0){
+        if (defaultAlignment <= 0) {
             defaultAlignment = NO_ALIGNMENT;
         }
         this.defaultAlignment = defaultAlignment;
     }
 
-    public void align(long offset, LocalFileHeader lfh){
-        if(ZipHeader.isZip64Length(offset + lfh.getSize())){
+    public void align(long offset, LocalFileHeader lfh) {
+        if (ZipHeader.isZip64Length(offset + lfh.getSize())) {
             return;
         }
         lfh.setZipAlign(0);
         lfh.updateDataDescriptor();
         int padding;
-        if(lfh.getMethod() == Archive.DEFLATED){
+        if (lfh.getMethod() == Archive.DEFLATED) {
             padding = 0;
-        }else {
+        } else {
             int alignment = getAlignment(lfh.getFileName());
             if (alignment == NO_ALIGNMENT) {
                 padding = 0;
@@ -73,26 +90,41 @@ public class ZipAligner {
         }
         lfh.setZipAlign(padding);
     }
-    private int getAlignment(String name){
-        if(!alignmentMap.isEmpty()) {
-            for(Map.Entry<Pattern, Integer> entry:alignmentMap.entrySet()){
-                Matcher matcher = entry.getKey().matcher(name);
-                if(matcher.matches()){
+    private int getAlignment(String name) {
+        if (!alignmentMap.isEmpty()) {
+            for (Map.Entry<Predicate<String>, Integer> entry: alignmentMap.entrySet()) {
+                Predicate<String> predicate = entry.getKey();
+                if (predicate.test(name)) {
                     return entry.getValue();
                 }
             }
         }
+        int alignment = getAlignmentDepreciated(name);
+        if (alignment != 0) {
+            return alignment;
+        }
         return defaultAlignment;
     }
 
-    public static ZipAligner apkAligner(){
+    private int getAlignmentDepreciated(String name) {
+        if (!alignmentPatternMapDepreciated.isEmpty()) {
+            for (Map.Entry<Pattern, Integer> entry: alignmentPatternMapDepreciated.entrySet()) {
+                Matcher matcher = entry.getKey().matcher(name);
+                if (matcher.matches()) {
+                    return entry.getValue();
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static ZipAligner apkAligner() {
         ZipAligner zipAligner = new ZipAligner();
         zipAligner.setDefaultAlignment(ALIGNMENT_4);
-        Pattern patternNativeLib = Pattern.compile("^lib/.+\\.so$");
-        zipAligner.setFileAlignment(patternNativeLib, ALIGNMENT_PAGE);
+        zipAligner.setFileAlignment(PREDICATE_NATIVE_LIBS, ALIGNMENT_PAGE_4KB);
         return zipAligner;
     }
-    public static ZipAligner noAlignment(){
+    public static ZipAligner noAlignment() {
         ZipAligner zipAligner = new ZipAligner();
         zipAligner.setDefaultAlignment(NO_ALIGNMENT);
         return zipAligner;
@@ -100,5 +132,8 @@ public class ZipAligner {
 
     private static final int NO_ALIGNMENT = 1;
     private static final int ALIGNMENT_4 = 4;
-    private static final int ALIGNMENT_PAGE = 4096;
+    private static final int ALIGNMENT_PAGE_4KB = 4096;
+
+    public static final Predicate<String> PREDICATE_NATIVE_LIBS = name ->
+            name.startsWith("lib/") && name.endsWith(".so");
 }
