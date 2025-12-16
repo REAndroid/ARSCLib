e2ec1b89b6235d4dd0e7fb8c463aae41f78c35c2
REAndroid <REAndroid@users.noreply.github.com>
2025-12-16 15:10:50 +0100

    Use File for LinkedIteratorTest

--- src/test/java/com/reandroid/utils/collection/LinkedIteratorTest.java
+++ src/test/java/com/reandroid/utils/collection/LinkedIteratorTest.java
@@ -3,60 +3,31 @@ package com.reandroid.utils.collection;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.Iterator;
 import java.util.List;
 
 public class LinkedIteratorTest {
 
     @Test
-    public void testLinkedItem() {
-        LinkedItem item =
-                new LinkedItem(null, "H")
-                .newChild("G")
-                .newChild("F")
-                .newChild("E")
-                .newChild("D")
-                .newChild("C")
-                .newChild("B")
-                .newChild("A");
+    public void testFileParents() {
+        File file = new File(
+                File.separatorChar + "H" +
+                        File.separatorChar + "G" +
+                        File.separatorChar + "F" +
+                        File.separatorChar + "E" +
+                        File.separatorChar + "D" +
+                        File.separatorChar + "C" +
+                        File.separatorChar + "B" +
+                        File.separatorChar + "A");
 
-        Iterator<LinkedItem> iterator = LinkedIterator.of(item, LinkedItem::getParent);
-        List<LinkedItem> itemList = CollectionUtil.collect(iterator);
-        Assert.assertEquals("itemList size", 7, itemList.size());
-        iterator = LinkedIterator.of(true, item, LinkedItem::getParent);
+        Iterator<File> iterator = LinkedIterator.of(file, File::getParentFile);
+        List<File> fileList = CollectionUtil.collect(iterator);
+        Assert.assertEquals("itemList size", 8, fileList.size());
 
-        List<LinkedItem> itemListIncludingSelf = CollectionUtil.collect(iterator);
-        Assert.assertEquals("itemListIncludingSelf size", 8, itemListIncludingSelf.size());
-    }
+        iterator = LinkedIterator.of(true, file, File::getParentFile);
 
-    static class LinkedItem {
-
-        public final String name;
-        private final LinkedItem parent;
-
-        public LinkedItem(LinkedItem parent, String name) {
-            this.parent = parent;
-            this.name = name;
-        }
-
-        public LinkedItem newChild(String name) {
-            return new LinkedItem(this, name);
-        }
-
-        public String getName() {
-            return name;
-        }
-        public LinkedItem getParent() {
-            return parent;
-        }
-
-        @Override
-        public String toString() {
-            LinkedItem parent = getParent();
-            if (parent == null) {
-                return getName();
-            }
-            return parent + "/" + getName();
-        }
+        List<File> fileListIncludingSelf = CollectionUtil.collect(iterator);
+        Assert.assertEquals("fileListIncludingSelf size", 9, fileListIncludingSelf.size());
     }
 }
